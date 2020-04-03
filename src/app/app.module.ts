a9506836e64b2a92ed4c68466d2f612ce6f97a57
xiaobian233 <1282845084@qq.com>
2020-04-03 15:25:49 +0800

    初始化

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
+//angualr依赖
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
+//ionic依赖
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+//入口模块/组件
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
